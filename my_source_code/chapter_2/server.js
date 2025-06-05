ea8047a0617f2cbb6af3744c86b69d64163db41b
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-06-05 14:22:39 +0200

    Update HTMX.

---
+++ my_source_code/chapter_2/server.js
@@ -0,0 +1,25 @@
+import express, { request } from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.post("/calculate", (req, res) => {
+    const height = parseFloat(req.body.height);
+    const weight = parseFloat(req.body.weight);
+    
+    const bmi = weight/(height * height);
+    const myName = req.body.myName
+    res.send(`
+        <h1>${myName}</h1><p>Height of ${height}m and Weight of ${weight}kg gives a BMI of ${bmi.toFixed(2)}kg/m²</p>
+    `);
+
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port: 3000");
+});
